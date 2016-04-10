e96fd240c5911e18f43c62b20ae5f18600c3d793
Markus Doits <markus.doits@stellenticket.de>
2016-04-10 21:37:18 +0200

    modify spec to check for .es6 liniting

--- spec/pronto/eslint_spec.rb
+++ spec/pronto/eslint_spec.rb
@@ -17,12 +17,12 @@ module Pronto
         it { should == [] }
       end
 
-      context 'patches with a four warnings' do
+      context 'patches with a four and a five warnings' do
         include_context 'test repo'
 
         let(:patches) { repo.diff('master') }
 
-        its(:count) { should == 4 }
+        its(:count) { should == 9 }
         its(:'first.msg') { should == "Expected { after 'if' condition." }
       end
     end
